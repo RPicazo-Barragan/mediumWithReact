1136c842afe7c7de694075474e342cfadc4fd3a7
RPicazo-Barragan <picazoricardo09@gmail.com>
2020-08-19 19:19:00 -0500

    Medium

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+//screens
+import Home from './screens/Home'
+import PostById from './screens/PostById'
+import PostPopular from './screens/PostPopular'
+import Form from './screens/Form'
+
+//css
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <Switch>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+          <Route path='/popular' exact>
+            <PostPopular />
+          </Route>
+
+          <Route path='/form' exact>
+            <Form />
+          </Route>
+          <Route path='/:id' exact>
+            <PostById />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
